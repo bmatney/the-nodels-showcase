be066fa7c01d3cfbcddd8b6beaacda27ac2d0e1c
Brian Matney <brianamatney@gmail.com>
2016-11-10 10:35:29 -0600

    added getLikes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,52 @@
-$(document).ready(function(){
-  console.log("document ready");
+$(document).ready(function() {
+    console.log("document ready");
 
-  getBios();
+    getBios();
 
-  function getBios(){
-
-  $.ajax({
-    type: 'GET',
-    url: '/bios',
-    success: function(bios){
-    console.log("get bios function");
-    console.log(bios);
-    appendBio(bios);
+    function getBios() {
+        $.ajax({
+            type: 'GET',
+            url: '/bios',
+            success: function(bios) {
+                console.log("get bios function");
+                console.log(bios);
+                appendBio(bios);
+            }
+        });
     }
-  });
-  }
 
-  $(".container").on('click', 'button', function(){
-    var namePath = '/likes/' + $(this).attr('id');
-    console.log(namePath);
-    $.ajax({
-      type: 'POST',
-      url: namePath,
-      success: function(data){
-        console.log(data);
-      }
-    })
-  });
+    function getLikes() {
+        $.ajax({
+            type: 'GET',
+            url: '/bios',
+            success: function(bios) {
+                console.log("update likes");
+                // updateLikes(bios);
+            }
+        });
+    }
 
-  function appendBio(bios) {
-    for (var i = 0; i < bios.length; i++) {
-      $('.row').append('<div class="col-md-4"><h2>' + bios[i].name + '</h2><p>' + bios[i].biography + '</p></div>');
+
+    $(".container").on('click', 'button', function() {
+        var namePath = '/likes/' + $(this).attr('id');
+        console.log(namePath);
+        $.ajax({
+            type: 'POST',
+            url: namePath,
+            success: function(data) {
+                console.log(data);
+                getLikes();
+            }
+        })
+    });
+
+    function appendBio(bios) {
+        for (var i = 0; i < bios.length; i++) {
+            $('.row').append('<div class="col-md-4"><h2>' + bios[i].name + '</h2><p>' + bios[i].biography + '</p></div>');
+        }
+        $('.container').append('<div class="row"></div>')
+        for (var i = 0; i < bios.length; i++) {
+            $('.row:last').append('<div class="col-md-4"><img src="' + bios[i].imgURL + '" /><br><button id="' + bios[i].firstName + '">Like | <span class="' + bios[i].firstName + '">0</span></button></div>');
+        }
     }
-    $('.container').append('<div class="row"></div>')
-    for (var i = 0; i < bios.length; i++)  {
-      $('.row:last').append('<div class="col-md-4"><img src="' + bios[i].imgURL + '" /><br><button id="' + bios[i].firstName + '">Like | <span class="' + bios[i].firstName + '">0</span></button></div>');
-    }
-  }
 });
